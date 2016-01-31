f1abbfe0804ff0ce4cff556ed4c7e797fdd476f5
Thomas Mayer <thomas@residuum.org>
2016-01-31 02:42:12 +0100

    Begin WPF application.

---
+++ CsharpDesktop/Desktop/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Configuration;
+using System.Windows;
+using System.IO;
+using System.Reflection;
+using Desktop.ViewModels;
+using PremierLeagueTable;
+using PremierLeagueTable.WebData;
+
+namespace Desktop
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        readonly Controller _controller;
+        readonly WindowModel _model = new WindowModel();
+        public MainWindow()
+        {
+            InitializeComponent();
+            _controller =new Controller(AssetsFolder);
+            BindController();
+            DataContext = _model;
+        }
+
+        void BindController()
+        {
+            _controller.Downloaded += ((sender, eventargs) =>
+            {
+                _table = eventargs.Table;
+                Dispatcher.BeginInvoke((Action) (() =>
+                {
+                    _model.ClearTeams();
+                    SonifyBtn.IsEnabled = true;
+                    _model.SetTeams(_table.Teams);
+                }));
+            });
+            }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            base.OnClosed(e);
+            _controller.Dispose();
+
+            Application.Current.Shutdown();
+        }
+
+
+        static string AssetsFolder
+        {
+            get
+            {
+                string assemblyFolder = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+                return Path.GetFullPath(assemblyFolder + ConfigurationManager.AppSettings["baseFolder"]);
+            }
+        }
+
+        void LoadData(object sender, RoutedEventArgs e)
+        {
+            SonifyBtn.IsEnabled = false;
+            _controller.Download();
+        }
+
+        void Sonify(object sender, RoutedEventArgs e)
+        {
+            _model.ClearTeams();
+            SonifyBtn.IsEnabled = false;
+            LoadBtn.IsEnabled = false;
+            _controller.Sonify(_table);
+        }
+
+        Table _table;
+    }
+}
---
+++ CsharpDesktop/Desktop/ViewModels/WindowModel.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Desktop.Annotations;
+using PremierLeagueTable.WebData;
+
+namespace Desktop.ViewModels
+{
+    class WindowModel :INotifyPropertyChanged
+    {
+        readonly ObservableCollection<Team> _teams = new ObservableCollection<Team>();
+        
+        public void ClearTeams()
+        {
+            _teams.Clear();
+            OnPropertyChanged(nameof(Teams));
+        }
+
+        public ObservableCollection<Team> Teams
+        {
+            get { return _teams; }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            if (propertyName == null || PropertyChanged == null)
+            {
+                return;
+            }
+            PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public void SetTeams(IEnumerable<Team> teams)
+        {
+            _teams.Clear();
+            foreach (var team in teams)
+            {
+                _teams.Add(team);
+            }
+            OnPropertyChanged(nameof(Teams));
+        }
+    }
+}
